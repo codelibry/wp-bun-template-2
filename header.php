35d967b660790a152f22b439841895ac9ec1330a
arma <montymo87@gmail.com>
2025-02-17 19:16:47 +0200

    fix header

--- header.php
+++ header.php
@@ -1,29 +1,3 @@
-<!--
-███╗   ███╗ █████╗ ██████╗ ███████╗    ██████╗ ██╗   ██╗
-████╗ ████║██╔══██╗██╔══██╗██╔════╝    ██╔══██╗╚██╗ ██╔╝
-██╔████╔██║███████║██║  ██║█████╗      ██████╔╝ ╚████╔╝
-██║╚██╔╝██║██╔══██║██║  ██║██╔══╝      ██╔══██╗  ╚██╔╝
-██║ ╚═╝ ██║██║  ██║██████╔╝███████╗    ██████╔╝   ██║
-╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝    ╚═════╝    ╚═╝
-
- ██████╗ ██████╗ ██████╗ ███████╗██╗     ██╗██████╗ ██████╗ ██╗   ██╗
-██╔════╝██╔═══██╗██╔══██╗██╔════╝██║     ██║██╔══██╗██╔══██╗╚██╗ ██╔╝
-██║     ██║   ██║██║  ██║█████╗  ██║     ██║██████╔╝██████╔╝ ╚████╔╝
-██║     ██║   ██║██║  ██║██╔══╝  ██║     ██║██╔══██╗██╔══██╗  ╚██╔╝
-╚██████╗╚██████╔╝██████╔╝███████╗███████╗██║██████╔╝██║  ██║   ██║
- ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝
-
--->
-
-<?php
-// SET DARK MODE IF SAVED IN COOKIE
-$mode = null;
-
-if (!empty($_COOKIE['mode'])) {
-	$mode = $_COOKIE['mode'];
-}
-?>
-
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> data-mode="light">
 
@@ -41,11 +15,7 @@ if (!empty($_COOKIE['mode'])) {
 	<?php
 	the_field('body_scripts_top', 'option');
 	function_exists('wp_body_open') ? wp_body_open() : do_action('wp_body_open');
-	?>
 
-	<div class="wrapper">
-
-		<?php
-		get_template_part('template-parts/skip-links');
-		get_template_part('template-parts/header/header');
-		get_template_part('template-parts/breadcrumbs');
+	get_template_part('template-parts/skip-links');
+	get_template_part('template-parts/header/header');
+	get_template_part('template-parts/breadcrumbs');
