cb922f943ddd1ebc9cae6f0ec2109bd807f0b453
arma <montymo87@gmail.com>
2025-05-31 14:47:56 +0300

    remove image and svg builder remove static folder and etc

--- dev.mjs
+++ dev.mjs
@@ -6,71 +6,71 @@ import { Bundler } from 'bun-bundler';
 // import { Bundler } from 'bun-bundler/modules';
 // import { Server, Bundler } from 'bun-bundler/modules';
 
-function getProjectFolderName() {
-	const currentDir = process.cwd();
-	const folderName = path.basename(currentDir);
-	return folderName;
+function dev() {
+	function getProjectFolderName() {
+		const currentDir = process.cwd();
+		const folderName = path.basename(currentDir);
+		return folderName;
+	}
+
+	const PROJECT_FOLDER = getProjectFolderName();
+
+	const debugMode = false;
+	const bundler = new Bundler();
+
+	const root = path.resolve('./');
+	const src = path.resolve('./src');
+	const dist = path.resolve('./dist');
+
+	const directories = {
+		src: src,
+
+		sass: [path.resolve(src, './scss/app.scss')],
+		js: [path.resolve(src, './js/app.js')],
+		// statics: path.resolve(src, './static/'),
+
+		dist: dist,
+		htmlDist: dist,
+		cssDist: path.resolve(dist, './css/'),
+		jsDist: path.resolve(dist, './js/'),
+	};
+
+	bundler.watch({
+		...directories,
+		production: process.env.NODE_ENV === 'production',
+		debug: debugMode,
+
+		onStart: () => {
+			browserSync.init({
+				proxy: 'http://template-v2/',
+				port: 3000,
+				injectChanges: true,
+				files: [
+					`${dist}/css/*.css`,
+					`${root}/**/*.php`,
+					`${src}/js/**/*.js`,
+					// `${src}/**/*`,
+					`!${root}/node_modules/**/*`,
+					`!${root}/helpers/**/*`,
+				],
+				open: true,
+				notify: false,
+				ghostMode: false,
+				reloadDebounce: 100,
+				// reloadOnRestart: true, // Перезагрузка при перезапуске
+			});
+		},
+		onBuildComplete: () => {
+			const criticalPath = path.resolve(src, './scss/critical/critical.scss');
+			if (fs.existsSync(criticalPath)) {
+				const result = sass.compile(criticalPath, { style: 'compressed', quietDeps: true });
+				fs.writeFileSync(path.join(`${dist}/css/`, 'critical.css'), result.css);
+			}
+		},
+		onCriticalError: () => {
+			server.stopServer();
+		},
+	});
 }
 
-const PROJECT_FOLDER = getProjectFolderName();
-
-const debugMode = false;
-const bundler = new Bundler();
-
-const root = path.resolve('./');
-const src = path.resolve('./src');
-const dist = path.resolve('./dist');
-
-const directories = {
-	src: src,
-
-	sass: [path.resolve(src, './scss/app.scss')],
-	js: [path.resolve(src, './js/app.js')],
-	images: path.resolve(src, './images/'),
-	fonts: path.resolve(root, 'assets/fonts/'),
-	statics: path.resolve(src, './static/'),
-
-	dist: dist,
-	htmlDist: dist,
-	cssDist: path.resolve(dist, './css/'),
-	jsDist: path.resolve(dist, './js/'),
-	imagesDist: path.resolve(dist, './images/'),
-	spriteDist: path.resolve(dist, './images/sprite'),
-};
-
-bundler.watch({
-	...directories,
-	production: process.env.NODE_ENV === 'production',
-	debug: debugMode,
-
-	onStart: () => {
-		browserSync.init({
-			proxy: 'http://template-v2/',
-			port: 3000,
-			injectChanges: true,
-			files: [
-				`${dist}/css/*.css`,
-				`${root}/**/*.php`,
-				`${src}/js/**/*.js`,
-				// `${src}/**/*`,
-				`!${root}/node_modules/**/*`,
-				`!${root}/helpers/**/*`,
-			],
-			open: true,
-			notify: false,
-			ghostMode: false,
-			reloadDebounce: 100,
-			// reloadOnRestart: true, // Перезагрузка при перезапуске
-		});
-	},
-	onBuildComplete: () => {
-		const criticalPath = path.resolve(src, './scss/critical/critical.scss');
-		if (fs.existsSync(criticalPath)) {
-			const result = sass.compile(criticalPath, { style: 'compressed', quietDeps: true });
-			fs.writeFileSync(path.join(`${dist}/css/`, 'critical.css'), result.css);
-		}
-	},
-	onCriticalError: () => {
-		server.stopServer();
-	},
-});
+dev();
--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,4 @@
 import { Bundler } from '../index.mjs';
 import { Server } from './server';
-import { SpriteBuilder } from './sprite-builder';
-import { ImageProcessor } from './image-processor';
 
-export { Bundler, Server, SpriteBuilder, ImageProcessor };
+export { Bundler, Server };
